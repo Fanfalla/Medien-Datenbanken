2138c51c872eb102eb04286c2d23bebabb178c02
Andreas Roloff <LightCr3ep3r2@gmai.com>
2022-12-06 00:48:00 +0100

    das existiert ja auch noch

--- InDatenbankSchreiben.js
+++ InDatenbankSchreiben.js
@@ -1,22 +1,24 @@
 function AnimeHinzufuegen() {
 
-    var Romaji = document.getElementById("Romaji").value
-    var Englisch = document.getElementById("Englisch").value
-    let Deutsch = document.getElementById("Deutsch").value
-    let Beschreibung = document.getElementById("Beschreibung").value
-    let FolgenAnzahl = document.getElementById("FolgenAnzahl").value
-    let FolgenDauer = document.getElementById("FolgenDauer").value
-    let Format = document.getElementById("Format").value
-    let Season = document.getElementById("Season").value
-    let StartDate = document.getElementById("StartDate").value
-    let EndDate = document.getElementById("EndDate").value
-    let Studio = document.getElementById("Studio").value
-    let Source = document.getElementById("Source").value
-    let Cover = document.getElementById("Cover").value
-    let Diashow = document.getElementById("Diashow").value
+    
+        var Romaji = document.getElementById("Romaji").value
+        var Englisch = document.getElementById("Englisch").value
+        let Deutsch = document.getElementById("Deutsch").value
+        let Beschreibung = document.getElementById("Beschreibung").value
+        let FolgenAnzahl = document.getElementById("FolgenAnzahl").value
+        let FolgenDauer = document.getElementById("FolgenDauer").value
+        let Format = document.getElementById("Format").value
+        let Season = document.getElementById("Season").value
+        let StartDate = document.getElementById("StartDate").value
+        let EndDate = document.getElementById("EndDate").value
+        let Studio = document.getElementById("Studio").value
+        let Source = document.getElementById("Source").value
+        let Cover = document.getElementById("Cover").value
+        let Diashow = document.getElementById("Diashow").value
 
-    let checked = document.querySelectorAll("#Genre :checked")
-    let selected = [...checked].map(option => option.value)
+        let checked = document.querySelectorAll("#Genre :checked")
+        let selected = [...checked].map(option => option.value)
+
 
     EmptyAnime()
 
